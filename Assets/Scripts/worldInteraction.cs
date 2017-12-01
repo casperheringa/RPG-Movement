1ce35903a37751102b3cba036053e0bd4a9b59b2
casperheringa <5be8141b@opayq.com>
2017-12-01 16:11:18 +0100

    RPG Movement
    A RPG movement system

---
+++ Assets/Scripts/worldInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class worldInteraction : MonoBehaviour {
+    NavMeshAgent playerAgent;
+
+    [SerializeField]
+    GameObject ping;
+    GameObject currentPing;
+
+    void Start()
+    {
+
+        playerAgent = GetComponent<NavMeshAgent>();
+        currentPing = Instantiate(ping, transform.position, Quaternion.identity);
+
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
+
+        currentPing.SetActive(playerAgent.remainingDistance > 0.5f);
+        	
+	}
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObj = interactionInfo.collider.gameObject;
+            playerAgent.destination = currentPing.transform.position = interactionInfo.point;
+        }
+    }
+
+}
